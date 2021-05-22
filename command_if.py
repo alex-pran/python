0c3d9473219af7a045a5ab30dbdbc3bc97ec1ed5
alex-pran <apranitchii@gmail.com>
2021-05-22 11:28:22 -0400

    update

---
+++ command_if.py
@@ -0,0 +1,13 @@
+age = 19
+if age >= 18:
+    print("You are old enough to vote! - 19")
+    
+else:
+    print("You are young enough to vote! - 17")
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+
+else:
+    print("You are young enough to vote! - 17")
