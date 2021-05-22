5b5feb858ce8bc421572f593ff4f92a8d5f4c0b4
alex-pran <apranitchii@gmail.com>
2021-05-22 14:57:18 -0400

    update2

--- toppings.py
+++ toppings.py
@@ -22,3 +22,48 @@ elif 'pepperoni' in requested_toppings:
 elif 'extra cheese' in requested_toppings:
     print("Adding extra cheese.")
 print("\nFinished making your pizza!")
+
+
+print("\n\n======================== new 2 =================")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
+
+print("\n\n======================== new 2 =================")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
+
+
+print("\n\n======================== new 2 =================")
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making pizza!")
+else:
+    print("Are you sure you want plain pizza?")
+
+print("\n\n======================== new 3 =================")
+
+available_toppings = ['mushrooms', 'olives', 'green pepeprs', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+
+print("\nFinished making your pizza.")
