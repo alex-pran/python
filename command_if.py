217884afe4eb9e1276c2a87e23127ee3afe24400
alex-pran <apranitchii@gmail.com>
2021-05-22 14:32:59 -0400

    update

--- command_if.py
+++ command_if.py
@@ -5,6 +5,8 @@ if age >= 18:
 else:
     print("You are young enough to vote! - 17")
 
+
+print("\n\n====================== new ======================")
 age = 17
 if age >= 18:
     print("You are old enough to vote!")
--- toppings.py
+++ toppings.py
@@ -10,3 +10,15 @@ if 'extra_cheese' in requested_topping:
     print("Adding extra cheese")
 
 print("\nFinished making your pizza.")
+
+
+print("\n\n======================== new =================")
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
