877e51df47240ebd8c2b0ef35d8a03f04f8e9736
alex-pran <apranitchii@gmail.com>
2021-05-23 17:56:50 -0400

    update3

---
+++ ex_5.8_hello_admin.py
@@ -0,0 +1,35 @@
+print("\n\n====================== 5.8 ===================")
+
+
+names = ['leo', 'kate', 'tom', 'john', 'admin']
+for name in names:
+    if name == 'admin':
+        print(f"Hello {name}, would you like to see a status report?")
+    else:
+        print(f"Hello {name}, thank you for logging in again")
+
+print("\n\n====================== 5.9 ===================")
+
+hello_admin = ['leo', 'kate', 'tom', 'john', 'admin']
+#hello_admin = []
+if hello_admin:
+    for hello_admins in hello_admin:
+        print(f"Added {hello_admins.title()}")
+    print("\nReady")
+else:
+    print("We need to add some users!")
+
+
+print("\n\n====================== 5.10 ===================")
+
+
+current_users = ['Leo', 'leo', 'marko', 'rob', 'KATE', 'teddy', 'ronald', 'jeff', 'kate', 'Kate', 'LEO']
+new_users = ['Leo', 'kate', 'tom', 'john', 'admin']
+
+for new_user in new_users:
+    if new_user in current_users:
+        print(f"{new_user.title()} use anothe name")
+    else:
+        print(f"{new_user.title()} you can use this name")
+
+print("\nYou signied")
