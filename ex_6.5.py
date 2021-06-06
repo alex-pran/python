1f44cbde8feee14b900fb61235b078af993ca383
alex-pran <apranitchii@gmail.com>
2021-06-06 18:33:33 -0400

    update3

--- ex_6.5.py
+++ ex_6.5.py
@@ -11,3 +11,13 @@ print(f"The Volga runs through {river_3}")
 for key, value in big_river.items():
     print(f"\nKey: {key}")
     print(f"Value: {value}")
+
+
+
+print("\n\n================== 9090909 ===================")
+
+users = {'name': 'ron', 'salary': 2000, 'age': 25,'name': 'tom', 'salary': 1000, 'age': 21,'name': 'sarah', 'salary': 2500, 'age': 23,'name': 'mike', 'salary': 3200, 'age': 28, 'name': 'lex', 'salary': 5000, 'age': 41,'name': 'timmy', 'salary': 900, 'age': 19}
+def get_data_for_sort(x):
+    return x('salary')
+
+sorted(users, key=get_data_for_sort)
