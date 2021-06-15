8a59facd6cb6f757066b0cbb2e94033597670bc6
alex-pran <apranitchii@gmail.com>
2021-06-15 19:22:01 -0400

    6.8.2021

--- ex_6.1.py
+++ ex_6.1.py
@@ -36,3 +36,28 @@ for key, value in human.items():
     print(f"Value: {value}")
 
 print("\n\n ==================== 6.4 ======================")
+
+print("\n\n ==================== 6.7 ======================")
+
+people = {'artkatty':{'firstname': 'katty', 'lastname': 'shishkina', 'location': 'brooklyn'},
+           'apranitchii':{'firstname': 'alexandr', 'lastname': 'pranitchii', 'location': 'brooklyn'},
+          'jdoe': {'firstname': 'john', 'lastname': 'doe', 'age': 24, 'location': 'atlanta'}}
+
+for usernames, users_info in people.items():
+    print(f"\nUsername: {usernames}")
+    fullysh_name = f"{users_info['firstname']} {users_info['lastname']}"
+    locations = f"{users_info['location']}"
+    print(f"\tFull name: {fullysh_name.title()}")
+    print(f"\tLocation: {locations.title()}")
+
+
+print("\n\n ==================== 6.8 ======================")
+
+pets = {'name1': {'cat': 'fox', 'bridal': 'scotisch', 'owner': 'KA'},
+        'name2': {'cat': 'fluffy', 'bridal': 'empty', 'owner': 'KA'}}
+
+for pet, pets_info in pets.items():
+    print(f"Pet: {pet}")
+    full_info = f"{pets_info['cat']} {pets_info['bridal']} {pets_info['owner']}"
+    print(f"About pet: {full_info}")
+
